7fc9c5919406b02168b0d77f675b542da508fdaf
GoncharovMikhail <godmihovel@gmail.com>
2021-12-28 02:51:12 +0300

    working on tests

--- pkg/db/user/impl/sql/pg_test.go
+++ pkg/db/user/impl/sql/pg_test.go
@@ -1,32 +1,99 @@
 package sql
 
 import (
-	"database/sql"
-	db "github.com/GoncharovMikhail/go-sql/pkg/db/user"
-	uuid "github.com/jackc/pgtype/ext/gofrs-uuid"
+	"context"
+	"fmt"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"os"
+	"strings"
 	"testing"
 )
 
-/*type pgContainer struct {
+type pgContainer struct {
 	testcontainers.Container
 }
-*/
-//var pgC *pgContainer = nil
 
+var (
+	packageName = "db"
+
+	//lateInit
+	pgC *pgContainer
+)
+
+// Create the Postgres TestContainer
 func init() {
 
+	workingDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	rootDir := strings.Replace(workingDir, packageName, "", 1)
+	mountFrom := fmt.Sprintf("%sresources/migrations/001__schema.up.sql", rootDir)
+	mountTo := "/docker-entrypoint-initdb.d/init.sql"
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "postgres:11.6-alpine",
+		ExposedPorts: []string{"5432/tcp"},
+		BindMounts:   map[string]string{mountFrom: mountTo},
+		Env: map[string]string{
+			"POSTGRES_DB": os.Getenv("DBNAME"),
+		},
+		WaitingFor: wait.ForLog("database system is ready to accept connections"),
+	}
+	tcC, err := testcontainers.GenericContainer(
+		ctx,
+		testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	pgC = &pgContainer{tcC}
+	print(pgC)
 }
 
-func TestSaveUser(t *testing.T) {
-	var user *db.UserEntity = &db.UserEntity{
-		Id:       uuid.UUID{},
-		Username: "test",
-		Password: "test",
-		RestoreData: &db.RestoreData{
-			//UserId:      "",
-			Email:       "test",
-			PhoneNumber: sql.NullString{},
+func TestMain(m *testing.M) {
+	defer func(container pgContainer, ctx context.Context) {
+		_ = container.Terminate(ctx)
+	}(*pgC, context.Background())
+	// Work out the path to the 'scripts' directory and set mount strings
+	packageName := "database"
+	workingDir, _ := os.Getwd()
+	rootDir := strings.Replace(workingDir, packageName, "", 1)
+	mountFrom := fmt.Sprintf("%s/scripts/init.sql", rootDir)
+	mountTo := "/docker-entrypoint-initdb.d/init.sql"
+	// Create the Postgres TestContainer
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "postgres:11.6-alpine",
+		ExposedPorts: []string{"5432/tcp"},
+		BindMounts:   map[string]string{mountFrom: mountTo},
+		Env: map[string]string{
+			"POSTGRES_DB": os.Getenv("DBNAME"),
 		},
+		WaitingFor: wait.ForLog("database system is ready to accept connections"),
 	}
-	print(user)
+	postgresC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		// Panic and fail since there isn't much we can do if the container doesn't start
+		panic(err)
+	}
+	defer func(postgresC testcontainers.Container, ctx context.Context) {
+		err := postgresC.Terminate(ctx)
+		if err != nil {
+			panic(err)
+		}
+	}(postgresC, ctx)
+	// Get the port mapped to 5432 and set as ENV
+	p, _ := postgresC.MappedPort(ctx, "5432")
+	_ = os.Setenv("DBPORT", p.Port())
+	exitVal := m.Run()
+	os.Exit(exitVal)
+
 }
