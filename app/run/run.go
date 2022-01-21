b05261e7709b9b400c525e0b44389c9a4e29936d
sharded <godmihovel@gmail.com>
2022-01-21 11:01:35 -0500

    add: grafana, docker-compose refactor, .env files

--- app/run/run.go
+++ app/run/run.go
@@ -2,39 +2,9 @@ package run
 
 import (
 	builtInSql "database/sql"
-	"fmt"
-	"github.com/GoncharovMikhail/go-sql/delivery/http/user"
-	"github.com/GoncharovMikhail/go-sql/internal/db"
-	"github.com/GoncharovMikhail/go-sql/pkg/db/user/impl/sql"
-	userService "github.com/GoncharovMikhail/go-sql/pkg/service/user"
-	"github.com/GoncharovMikhail/go-sql/pkg/service/user/impl"
-	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v4"
-	"log"
 )
 
 func Run() error {
-	pg, err := db.GetDb(&pgx.ConnConfig{})
-	if err != nil {
-		log.Panicf("")
-	}
-	defer closeDb(pg)
-	ur := sql.postgresUserRepository{
-		Db: pg,
-	}
-	var us userService.UserService = &impl.UserServiceImpl{
-		Ur: &ur,
-	}
-	engine := gin.Default()
-	uc := user.UserController{
-		Engine: engine,
-		Us:     &us,
-	}
-	fmt.Println(uc)
-	errEngineRun := engine.Run(":8080")
-	if errEngineRun != nil {
-		return errEngineRun
-	}
 	return nil
 }
 
