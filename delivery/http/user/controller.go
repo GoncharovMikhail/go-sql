23b65ab46c8150ebed975c87f4f6d60531f9f74b
sharded <godmihovel@gmail.com>
2022-01-21 19:24:20 -0500

    refactor: ref

--- delivery/http/user/controller.go
+++ delivery/http/user/controller.go
@@ -1,9 +1,6 @@
 package user
 
 import (
-	"fmt"
-	webConsts "github.com/GoncharovMikhail/go-sql/const/main"
-	model "github.com/GoncharovMikhail/go-sql/model/user"
 	"github.com/GoncharovMikhail/go-sql/pkg/service/user"
 	"github.com/gin-gonic/gin"
 )
@@ -16,23 +13,3 @@ type UserController struct {
 const (
 	userPath = "user/"
 )
-
-func (uc *UserController) init() {
-	uc.
-		Engine.
-		PUT(
-			webConsts.ApiPath+userPath+"save",
-			func(ctx *gin.Context) {
-				var request *model.UserSaveRequest
-				err := ctx.BindJSON(request)
-				if err != nil {
-					panic(err)
-				}
-				save, err := (*uc.Us).Save(request)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println(save)
-			},
-		)
-}
