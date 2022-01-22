0287664ecf7818cb0e98a05368899aca27e05c53
sharded <godmihovel@gmail.com>
2022-01-21 19:25:41 -0500

    refactor: ref

--- pkg/service/user/users.go
+++ pkg/service/user/users.go
@@ -1,12 +1,1 @@
 package user
-
-/*import (
-	model "sql/model/user"
-	entity "sql/pkg/db/user"
-)
-
-type UserService interface {
-	Save(request *model.UserSaveRequest) (*entity.UserEntity, error)
-	FindOneByUsername(username string) (*entity.UserEntity, bool)
-}
-*/
