22327cd15008535d5dad19b185835908fb57606c
sharded <godmihovel@gmail.com>
2022-01-21 19:20:47 -0500

    refactor: main.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/jackc/pgx/v4"
 )
 
 func main() {
-	config, err := pgx.ParseConfig("postgresql://localhost:5432/postgres")
-	if err != nil {
-		panic(err)
-	}
-	config.User = "postgres"
-	config.Password = "postgres"
-	fmt.Println(config.User)
+	fmt.Printf("")
 }
