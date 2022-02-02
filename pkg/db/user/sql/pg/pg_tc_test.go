0d96cd02000f0c7608ad0533909d0edd179d2cdb
sharded <godmihovel@gmail.com>
2022-02-01 20:52:20 -0500

    refactor: ref

--- pkg/db/user/sql/pg/pg_tc_test.go
+++ pkg/db/user/sql/pg/pg_tc_test.go
@@ -1,7 +1,6 @@
 package pg
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"github.com/GoncharovMikhail/go-sql/const/test"
@@ -21,14 +20,20 @@ import (
 const (
 	version  = "14.1-alpine"
 	postgres = "postgres"
+	username = postgres
 	password = "password"
 	port     = "5432"
 )
 
-func CreateTestContainer(ctx context.Context, dbname string) (testcontainers.Container, *sql.DB, error) {
+var (
+	container testcontainers.Container
+	db        *sql.DB
+)
+
+func init() {
 	goModDir, err := util.GetGoModDir()
 	if err != nil {
-		return nil, nil, err
+		log.Panic(err)
 	}
 	initDbFiles, errors := util.ListAllFilesMatchingPatternsAllOverOsFromSpecifiedDir(
 		goModDir,
@@ -37,63 +42,51 @@ func CreateTestContainer(ctx context.Context, dbname string) (testcontainers.Con
 		".*/resources/migrations.*", "up.sql",
 	)
 	if errors != nil {
-		panic(errors)
+		log.Panic(errors)
 	}
 	//todo
 	initDbDir := filepath.Dir(initDbFiles[0])
-	var port = "5432/tcp"
-	dbURL := func(port nat.Port) string {
-		return fmt.Sprintf("postgres://postgres:password@localhost:%s/%s?sslmode=disable", port.Port(), dbname)
-	}
 
 	req := testcontainers.GenericContainerRequest{
 		ContainerRequest: testcontainers.ContainerRequest{
-			Image:        "postgres:latest",
+			Image:        postgres + ":" + version,
 			ExposedPorts: []string{port},
 			Cmd:          []string{"postgres", "-c", "fsync=off"},
 
 			Env: map[string]string{
-				"POSTGRES_DB":       dbname,
-				"POSTGRES_USER":     "postgres",
-				"POSTGRES_PASSWORD": "password",
+				"POSTGRES_DB":       postgres,
+				"POSTGRES_USER":     username,
+				"POSTGRES_PASSWORD": password,
 			},
 			BindMounts: map[string]string{
 				"/docker-entrypoint-initdb.d": initDbDir,
 			},
-			WaitingFor: wait.ForSQL(nat.Port(port), "postgres", dbURL).Timeout(time.Second * 5),
+			WaitingFor: wait.ForSQL(port, postgres, func(port nat.Port) string {
+				return fmt.Sprintf("%s://%s:%s@localhost:%s/%s?sslmode=disable", postgres, username, password, port.Port(), postgres)
+			}).Timeout(time.Second * 5),
 		},
 		Started: true,
 	}
-	container, err := testcontainers.GenericContainer(ctx, req)
+	container, err = testcontainers.GenericContainer(test.CTX, req)
 	if err != nil {
-		return container, nil, fmt.Errorf("failed to start container: %s", err)
+		log.Panicf("failed to start container: %s", err)
 	}
 
-	mappedPort, err := container.MappedPort(ctx, nat.Port(port))
+	mappedPort, err := container.MappedPort(test.CTX, port)
 	if err != nil {
-		return container, nil, fmt.Errorf("failed to get container external port: %s", err)
+		log.Panicf("failed to get container external port: %s", err)
 	}
 
 	log.Println("postgres container ready and running at port: ", mappedPort)
 
-	url := fmt.Sprintf("postgres://postgres:password@localhost:%s/%s?sslmode=disable", mappedPort.Port(), dbname)
-	db, err := sql.Open("postgres", url)
+	url := fmt.Sprintf("%s://%s:%s@localhost:%s/%s?sslmode=disable", postgres, username, password, mappedPort.Port(), postgres)
+	db, err = sql.Open("postgres", url)
 	if err != nil {
-		return container, db, fmt.Errorf("failed to establish database connection: %s", err)
+		log.Panicf("failed to establish database connection: %s", err)
 	}
-
-	return container, db, nil
 }
 
 func TestSaveInTx(t *testing.T) {
-	_, db, err := CreateTestContainer(test.CTX, postgres)
-	if err != nil {
-		panic(err)
-	}
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
 	tx := util.MustBeginTx(test.CTX, db, &sql.TxOptions{
 		Isolation: sql.LevelDefault,
 	})
